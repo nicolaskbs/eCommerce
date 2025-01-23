713faec83b0faa71b889ad8ef7d638a69acdb91d
Nicolas Kevin <nicolas.kevin.018@ufrn.edu.br>
2025-01-22 22:32:22 -0300

    Implementação Testes de Custo Total (com 1 produto e 1 item).

--- src/test/java/ecommerce/service/CompraServiceTest.java
+++ src/test/java/ecommerce/service/CompraServiceTest.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import ecommerce.entity.CarrinhoDeCompras;
 import ecommerce.entity.Cliente;
@@ -23,29 +25,70 @@ public class CompraServiceTest {
     @Autowired
     private CompraService compraService;
 
-    @Test
-	void testeCalcularCustoTotal() {
+    @ParameterizedTest
+    @CsvSource({
+            "OURO, 100, BigDecimal.ZERO", // Cliente Ouro, Peso qualquer (REGRA 1)
+            "BRONZE, 4, BigDecimal.ZERO", // Cliente qualquer, Peso <5 (REGRA 2)
+            "PRATA, 5, BigDecimal.valueOf(1)", // Cliente Prata, Peso <10 (REGRA 3)
+            "PRATA, 9, BigDecimal.valueOf(1)", // Cliente Prata, Peso <10 (REGRA 3)
+            "PRATA, 10, BigDecimal.valueOf(2)", // Cliente Prata, Peso <50 (REGRA 4)
+            "PRATA, 49, BigDecimal.valueOf(2)", // Cliente Prata, Peso <50 (REGRA 4)
+            "PRATA, 50, BigDecimal.valueOf(3.5)", // Cliente Prata, Peso >=50 (REGRA 5)
+            "PRATA, 51, BigDecimal.valueOf(3.5)", // Cliente Prata, Peso >=50 (REGRA 5)
+            "BRONZE, 5, BigDecimal.valueOf(2)", // Cliente Bronze, Peso <10 (REGRA 6)
+            "BRONZE, 9, BigDecimal.valueOf(2)", // Cliente Bronze, Peso <10 (REGRA 6)
+            "BRONZE, 10, BigDecimal.valueOf(4)", // Cliente Bronze, Peso <50 (REGRA 7)
+            "BRONZE, 49, BigDecimal.valueOf(4)", // Cliente Bronze, Peso <50 (REGRA 7)
+            "BRONZE, 50, BigDecimal.valueOf(7)", // Cliente Bronze, Peso >=50 (REGRA 8)
+            "BRONZE, 51, BigDecimal.valueOf(7)" // Cliente Bronze, Peso >=50 (REGRA 8)
+    })
+	void testeCalcularFrete(TipoCliente tipo, Integer peso, BigDecimal multiplicador) {
         // Inicializar cliente
-        Cliente cliente = new Cliente(1L, "João", "Rua 1", TipoCliente.OURO);
+        Cliente cliente = new Cliente(1L, "João", "Rua 1", tipo);
 
-        // Iniciarlizar produtos
-        Produto produto1 = new Produto(2L, "Produto 1", "Descrição 1", new BigDecimal("100.00"), 10, TipoProduto.ROUPA);
-        Produto produto2 = new Produto(3L, "Produto 2", "Descrição 2", new BigDecimal("50.00"), 5, TipoProduto.ALIMENTO);
+        // Inicializar produto
+        Produto produto1 = new Produto(2L, "Produto 1", "Descrição 1", new BigDecimal("100.00"), peso, TipoProduto.ROUPA);
 
         // Inicializar itens
-        ItemCompra item1 = new ItemCompra(2L, produto1, 5L);
-        ItemCompra item2 = new ItemCompra(3L, produto2, 8L);
+        ItemCompra item1 = new ItemCompra(3L, produto1, 1L);
         List<ItemCompra> itens = new ArrayList<>();
         itens.add(item1);
-        itens.add(item2);
 
         // Inicializar carrinho
         CarrinhoDeCompras carrinho = new CarrinhoDeCompras(4L, cliente, itens, null);
 
-		// Calcular resultado
-		BigDecimal resultado = compraService.calcularCustoTotal(carrinho);
+		// Calcular frete
+		BigDecimal frete = compraService.calcularFrete(carrinho);
 
-		assertEquals(resultado, new BigDecimal("1500.00"));
+		assertEquals(frete, multiplicador.multiply(BigDecimal.valueOf(peso)));
 	}
-    
+
+    @ParameterizedTest
+    @CsvSource({
+            "BigDecimal.valueOf(499.99), BigDecimal.valueOf(1)", // <=500 (REGRA 1)
+            "BigDecimal.valueOf(500), BigDecimal.valueOf(1)", // <=500 (REGRA 1)
+            "BigDecimal.valueOf(999.99), BigDecimal.valueOf(0.9)", // <=1000 (REGRA 2)
+            "BigDecimal.valueOf(1000), BigDecimal.valueOf(0.9)", // <=1000 (REGRA 2)
+            "BigDecimal.valueOf(1000.01), BigDecimal.valueOf(0.8)" // >1000 (REGRA 3)
+    })
+    void testeCalcularValorProdutos(BigDecimal valorBase, BigDecimal multiplicador) {
+        // Inicializar cliente
+        Cliente cliente = new Cliente(1L, "João", "Rua 1", TipoCliente.PRATA);
+
+        // Inicializar produto
+        Produto produto1 = new Produto(2L, "Produto 1", "Descrição 1", valorBase, 10, TipoProduto.ROUPA);
+
+        // Inicializar itens
+        ItemCompra item1 = new ItemCompra(3L, produto1, 1L);
+        List<ItemCompra> itens = new ArrayList<>();
+        itens.add(item1);
+
+        // Inicializar carrinho
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras(4L, cliente, itens, null);
+
+        // Calcular frete
+        BigDecimal valorProdutos = compraService.calcularValorProdutos(carrinho);
+
+        assertEquals(valorProdutos, valorBase.multiply(multiplicador));
+    }
 }
